845bcd9504a21473a2c7c9b85514fa3b21687460
Mallari1297 <130537446+Mallari1297@users.noreply.github.com>
2024-12-17 15:52:25 +0800

    fix

--- resources/views/linkage.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layouts.app')
-@section('title', 'Linkages')
-@section('content')
-    <h2>Our Linkages</h2>
-    <p>Eastwoods Professional College partners with various institutions and industries to ensure quality education and job placements for our graduates.</p>
-    <img src="{{ asset('images/linkages.jpg') }}" alt="Linkages">
-    <p>We value collaboration and strong connections in building your future.</p>
-@endsection
